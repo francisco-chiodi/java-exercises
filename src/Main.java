24c112ab2efb7c8590a193da927863697041e155
francisco-chiodi <franchiodi98@gmail.com>
2025-01-22 20:34:02 -0500

    #2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
-    public static main(String[] args) {
+    public static void main(String[] args) {
 
-      int[] numbers = {1,2,3,4,5};
-      
-      public static int sum (int numbers);
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        System.out.println("numbers is = " + sumArray(numbers));
+    }
+public static int sumArray(int [] arr ) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum;
 }
-
+}
