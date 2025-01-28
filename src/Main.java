15fdb18fda881270cbcfd3e85f49ff1badd2ad0a
francisco-chiodi <franchiodi98@gmail.com>
2025-01-27 19:17:06 -0500

    #2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
-public class Main {
+import static java.lang.Math.max;
+
+/*public class Main{
+    public static void main(String [] args){
+        int[] values = {10, 20, 30, 40, 50};
+
+        System.out.println("the bigger is = " + findMax(values));
+
+    }
+
+    public static int findMax(int [] array) {
+        int max = array [0];
+
+        for (int num : array) {
+            max = Math.max(max , num);
+        }
+        return max;
+    }
+}
+*/
+
+/*public class Main {
     public static void main(String[] args) {
 
-        int[] numbers = {1, 2, 3, 4, 5};
+       int[] numbers = {1, 2, 3, 4, 5};
 
         System.out.println("numbers is = " + sumArray(numbers));
     }
@@ -13,3 +34,5 @@ public static int sumArray(int [] arr ) {
         return sum;
 }
 }
+
+*/
